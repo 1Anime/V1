1c45a33bc035707b0732e1eab7d808a7c35f5be5
TBNRDev <108610517+TBNRDev@users.noreply.github.com>
2024-04-20 13:42:02 +0800

    Fix Ads Link

--- src/components/details/tabs/Overview.js
+++ src/components/details/tabs/Overview.js
@@ -112,7 +112,7 @@ function Overview({data}) {
                 
           {/* AD HERE */}
           <div className="ad-container">
-            <a href="#">
+            <a href="https://linktr.ee/1anime">
               <img src="https://media.discordapp.net/attachments/1001735268958752791/1231066896741175316/SupportUs.png?ex=662477e4&is=66232664&hm=a16531729b639675ce1f39b01fcf6360764525b26744fd4e3a37850ca7007e0e&=&format=webp&quality=lossless" alt="Support Us" className="ad-image" />
             </a>
           </div>
