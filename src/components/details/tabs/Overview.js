d0cd2a3671522ebf9c22cc776a284a2e62d5677b
TBNRDev <108610517+TBNRDev@users.noreply.github.com>
2024-04-20 14:52:39 +0800

    Update Images

--- src/components/details/tabs/Overview.js
+++ src/components/details/tabs/Overview.js
@@ -113,7 +113,7 @@ function Overview({data}) {
           {/* AD HERE */}
           <div className="ad-container">
             <a href="https://linktr.ee/1anime">
-              <img src="https://media.discordapp.net/attachments/1001735268958752791/1231066896741175316/SupportUs.png?ex=662477e4&is=66232664&hm=a16531729b639675ce1f39b01fcf6360764525b26744fd4e3a37850ca7007e0e&=&format=webp&quality=lossless" alt="Support Us" className="ad-image" />
+              <img src="https://media.discordapp.net/attachments/1001735269466259460/1231135029611204648/Ad.jpg?ex=6635dad8&is=662365d8&hm=5ec36eb079e0c60a801b51055c7e5ac81dd5e9e243a9784ada92bb20a4b30bc6&=&format=webp" alt="Support Us" className="ad-image" />
             </a>
           </div>
             </div>
