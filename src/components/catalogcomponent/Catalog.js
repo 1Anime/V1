449c29c2a983f6b563b74e67a000159f999d31a3
Dev1Anim <89574788+Dev1Anim@users.noreply.github.com>
2024-06-10 18:09:14 +0800

    Fix Search lol

--- src/components/catalogcomponent/Catalog.js
+++ src/components/catalogcomponent/Catalog.js
@@ -122,13 +122,6 @@ function Catalog({ searchParams }) {
                             <h3 className={styles.searchlabel}>Genres</h3>
                             <Combobox value={genrevalue} onChange={setGenrevalue} multiple>
                                 <div className="relative w-full cursor-default overflow-hidden rounded-[0.6rem] text-left shadow-md focus:outline-none sm:text-sm">
-                                    <Combobox.Input
-                                        className="w-full border-none py-[9px] pl-3 pr-10 text-sm leading-5 bg-[#27272a] text-[#b2b2b2] focus:ring-0 outline-none"
-                                        displayValue={(item) => item?.map((item) => item?.name).join(", ")}
-                                        placeholder="Select Genres"
-                                        onChange={(event) => setQuery(event.target.value)}
-                                        autoComplete="off"
-                                    />
                                     <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon" className="h-5 w-5 text-gray-400"><path fillRule="evenodd" d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z" clipRule="evenodd"></path></svg>
                                     </Combobox.Button>
@@ -215,106 +208,49 @@ function Catalog({ searchParams }) {
                         </div>
                         <div className={styles.catalogsort}>
                             <h3 className={styles.searchlabel}>Sort by</h3>
-                            <Select
-                                labelPlacement={"outside"}
-                                label=""
-                                aria-label="Sort by"
-                                placeholder="Sort by"
-                                selectedKeys={sortbyvalue}
-                                className="max-w-xs"
-                                onSelectionChange={setSortbyvalue}
-                            >
-                                {sortbyOptions.map((item) => (
                                     <SelectItem key={item.value} value={item.value}>
                                         {item.name}
                                     </SelectItem>
-                                ))}
-                            </Select>
-                        </div>
+                                                   </div>
                         <button className='hidden lg:flex items-end cursor-default' onClick={resetValues} disabled={isFormEmpty}>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 mb-2 cursor-pointer">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                             </svg>
                         </button>
-                        {/* <div className={styles.catalogsort}>
+                        <div className={styles.catalogsort}>
                         <h3 className={styles.searchlabel}>Airing Status</h3>
-                        <Select
-                            labelPlacement={"outside"}
-                            label=""
-                            aria-label="Sort by"
-                            placeholder="Select Status"
-                            selectedKeys={sortbyvalue}
-                            className="max-w-xs"
-                            onSelectionChange={setAiringvalue}
-                        >
-                            {airingOptions.map((item) => (
                                 <SelectItem key={item.value} value={item.value}>
                                     {item.name}
                                 </SelectItem>
-                            ))}
-                        </Select>
-                    </div> */}
+                    </div> 
                         <div className={styles.yearmobil}>
                             <h3 className={styles.searchlabel}>Year</h3>
-                            <Select
-                                label=""
-                                aria-label='year'
-                                labelPlacement='outside'
-                                placeholder="Select Year"
-                                className="w-full">
-                                {yearOptions.map((year) => (
-                                    <SelectItem
+                                                                <SelectItem
                                         key={year.value}
                                         onClick={() => handleYearClick(year.value)}
                                     >
                                         {year.name}
                                     </SelectItem>
-                                ))}
-                            </Select>
                         </div>
                     </div>
                     <div className={styles.bottomtwo}>
                         <div className={styles.yearmobil}>
                             <h3 className={styles.searchlabel}>Format</h3>
-                            <Select
-                                label=""
-                                aria-label='format'
-                                labelPlacement='outside'
-                                placeholder="Select Format"
-                                className="w-full"
-                                selectedKeys={formatvalue}
-                                onSelectionChange={setFormatvalue}
-                            >
-                                {formatOptions.map((format) => (
                                     <SelectItem
                                         key={format.value}
                                         value={format.value}
                                     >
                                         {format.name}
                                     </SelectItem>
-                                ))}
-                            </Select>
                         </div>
                         <div className={styles.yearmobil}>
                             <h3 className={styles.searchlabel}>Season</h3>
-                            <Select
-                                label=""
-                                aria-label='season'
-                                labelPlacement='outside'
-                                placeholder="Select Season"
-                                className="w-full"
-                                selectedKeys={seasonvalue}
-                                onSelectionChange={setSeasonvalue}
-                            >
-                                {seasonOptions.map((season) => (
                                     <SelectItem
                                         key={season.value}
                                         value={season.value}
                                     >
                                         {season.name}
                                     </SelectItem>
-                                ))}
-                            </Select>
                         </div>
                     </div>
                 </>}
