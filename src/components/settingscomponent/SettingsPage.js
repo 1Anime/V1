c1bf37e289201b57339d56588fbe35dcffc3e208
Dev1Anim <89574788+Dev1Anim@users.noreply.github.com>
2024-06-11 18:09:17 +0800

    rah

--- src/components/settingscomponent/SettingsPage.js
+++ src/components/settingscomponent/SettingsPage.js
@@ -43,6 +43,13 @@ function SettingsPage() {
                         <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                     </svg>
                     Settings</div>
+                    <div className='mx-3 bg-[#1a1a1f] px-5 py-3 rounded-lg text-[30px] text-bold flex flex-row items-center'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75m-3-7.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285Z" />
+</svg>
+                    Official Domains
+                    <p className='text-[11px] md:text-[13px] text-[#bfc6d0] lg:max-w-[55%] line-clamp-3'>Official Domains are: 1anime.co, 1anime.info, animeyee.lol (Proxy)</p></div>
+
             </div>
             <div className='max-w-[94%] md:max-w-[95%] lg:max-w-[85%] xl:max-w-[80%] mx-auto min-h-[58vh] flex flex-col gap-6 mt-5'>
                 {loading ? (
@@ -138,12 +145,6 @@ function SettingsPage() {
                                 />
                             </div>
                         </div>
-                        <div className='mx-3 bg-[#1a1a1f] px-5 py-3 rounded-lg text-[35px] text-bold flex flex-row items-center'>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75m-3-7.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285Z" />
-</svg>
-                    Official Domains
-                    <p className='text-[11px] md:text-[13px] text-[#bfc6d0] lg:max-w-[55%] line-clamp-3'>Official Domains are: 1anime.co, 1anime.info, animeyee.lol (Proxy)</p></div>
                     </>
                 )}
             </div>
