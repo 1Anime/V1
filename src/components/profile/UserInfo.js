47cea0915e7ade25e0e76847cdf12a0bcf0b4230
Dev1Anim <89574788+Dev1Anim@users.noreply.github.com>
2024-06-12 12:13:11 +0800

    Update UserInfo.js

--- src/components/profile/UserInfo.js
+++ src/components/profile/UserInfo.js
@@ -16,6 +16,12 @@ function UserInfo({ lists, session }) {
     return (
         <div>
             <div className="max-w-[95%] lg:max-w-[90%] xl:max-w-[86%] mx-auto">
+        
+                        <div className='mx-3 bg-[#1a1a1f] px-5 py-3 rounded-lg text-bold flex flex-row items-center'>
+                <svg width="25px" height="25px" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75m-3-7.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285Z" />
+</svg>
+                    <p className='text-[11px] md:text-[13px] text-[#bfc6d0] lg:max-w-[55%] line-clamp-3'> 1Anime secures your AniList account and never shares any info with other services, Free & Next-Level Security • Learn more.</p></div>
                 <div className="flex mb-3 flex-nowrap overflow-x-auto scrollbar-hide">
                     {lists.map((tab) => (
                         <div
