a7d50388448ed2ece6ee2b75a90c0ddc39e7fd54
Dev1Anim <89574788+Dev1Anim@users.noreply.github.com>
2024-06-23 15:10:45 +0800

    Update AuthPage.js

--- src/components/auth/AuthPage.js
+++ src/components/auth/AuthPage.js
@@ -72,7 +72,7 @@ function AuthPage() {
                             <svg width="25px" height="25px" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
   <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75m-3-7.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285Z" />
 </svg>
-                    <p className='text-[11px] md:text-[13px] text-[#bfc6d0] lg:max-w-[55%] line-clamp-3'> By signing up/logging in, you agree to our <a className="text-blue-500 hover:text-blue-600" href="">Privacy Policy + Terms of Service</a></p></div></div>
+                    <p className='text-[11px] md:text-[13px] text-[#bfc6d0] lg:max-w-[55%] line-clamp-3'> By signing up/logging in, you agree to our <a className="text-blue-500 hover:text-blue-600" href="https://1anime.tawk.help/category/policies">Privacy Policy + Terms of Service</a></p></div></div>
               </div>
                 </div></div></div>
                 )}
