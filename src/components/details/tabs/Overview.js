898c0ba91a696247b05cb354b93051db321430e3
Dev1Anim <89574788+Dev1Anim@users.noreply.github.com>
2024-06-24 18:01:01 +0800

    Try new UI Fix

--- src/components/details/tabs/Overview.js
+++ src/components/details/tabs/Overview.js
@@ -2,6 +2,7 @@
 import React, { useState } from 'react'
 import styles from '../../../styles/AnimeDetailsBottom.module.css'
 import { Tooltip } from "@nextui-org/react";
+import { ShareIcon } from "@heroicons/react/24/solid";
 
 function Overview({data}) {
     const [showFullDescription, setShowFullDescription] = useState(false);
@@ -30,6 +31,28 @@ function Overview({data}) {
         const month = ['Jan', 'Feb', 'Mar', 'Apr', 'May','Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
         return month[m]
     }
+
+    
+    const isAnime = data?.type === 'ANIME' || true;
+
+  
+    const handleShareClick = async () => {
+      try {
+        if (navigator.share) {
+          await navigator.share({
+            title: `${isAnime ? "Watch" : "Read"} Now - ${data?.title?.english}`,
+             text: `Watch [${data?.title?.romaji}] and more on 1Anime. Join us for endless anime entertainment`,
+            url: window.location.href,
+          });
+        } else {
+          // Web Share API is not supported, provide a fallback or show a message
+          alert("Web Share API is not supported in this browser.");
+        }
+      } catch (error) {
+        console.error("Error sharing:", error);
+      }
+    };
+  
 
     return (
         <div className={styles.detailscard}>
@@ -93,7 +116,27 @@ function Overview({data}) {
                 </div>
             </div>
             <div className={styles.card2}>
-                <h3 className={styles.detailsheading}>Description</h3>
+                <h3 className={styles.detailsheading}>Description</h3>   
+        <button
+            type="button"
+            className="${styles.detailswatch} bg-[#FFFFFF] text-black text-xs font-bold px-2 py-1 rounded-md"
+            onClick={handleShareClick}
+          >
+            <span className="absolute pointer-events-none z-40 opacity-0 -translate-y-8 group-hover:-translate-y-10 group-hover:opacity-100 font-karla shadow-tersier shadow-md whitespace-nowrap bg-secondary px-2 py-1 rounded transition-all duration-200 ease-out">
+              Share {isAnime ? "Anime" : "Manga"}
+            </span>
+            <ShareIcon className="w-5 h-5" />
+          </button>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href={`https://anilist.co/${data.type.toLowerCase()}/${data.id}`}
+            className="${styles.detailswatch} bg-[#FFFFFF] text-black text-xs font-bold px-2 py-1 rounded-md"
+          >
+            <span className="absolute pointer-events-none z-40 opacity-0 -translate-y-8 group-hover:-translate-y-10 group-hover:opacity-100 font-karla shadow-tersier shadow-md whitespace-nowrap bg-secondary px-2 py-1 rounded transition-all duration-200 ease-out">
+              See on AniList
+            </span>
+See on AL</a>
                 <div className={styles.descriptioncontent}>
                     <p dangerouslySetInnerHTML={{ __html: data?.description }} />
                 </div>
