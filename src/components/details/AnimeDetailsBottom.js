f0bcb629cf3510446b0ba298e276e55fa69c09bf
Dev1Anim <89574788+Dev1Anim@users.noreply.github.com>
2024-06-24 13:58:43 +0800

    Rah

--- src/components/details/AnimeDetailsBottom.js
+++ src/components/details/AnimeDetailsBottom.js
@@ -33,7 +33,7 @@ function AnimeDetailsBottom({ data }) {
 
   
   const isAnime = data?.type === 'ANIME' || true;
-  
+
   
   const handleShareClick = async () => {
     try {
@@ -85,7 +85,7 @@ function AnimeDetailsBottom({ data }) {
             <span className="absolute pointer-events-none z-40 opacity-0 -translate-y-8 group-hover:-translate-y-10 group-hover:opacity-100 font-karla shadow-tersier shadow-md whitespace-nowrap bg-secondary px-2 py-1 rounded transition-all duration-200 ease-out">
               See on AniList
             </span>
-<svg fill="#000000" width="25px" height="25px" viewBox="0 0 24 24" role="img" xmlns="http://www.w3.org/2000/svg"><path d="M6.361 2.943 0 21.056h4.942l1.077-3.133H11.4l1.052 3.133H22.9c.71 0 1.1-.392 1.1-1.101V17.53c0-.71-.39-1.101-1.1-1.101h-6.483V4.045c0-.71-.392-1.102-1.101-1.102h-2.422c-.71 0-1.101.392-1.101 1.102v1.064l-.758-2.166zm2.324 5.948 1.688 5.018H7.144z"/></svg></a>
+AL</a>
         <AnimatePresence mode="wait">
           <motion.div
             key={activeTab.name || "empty"}
