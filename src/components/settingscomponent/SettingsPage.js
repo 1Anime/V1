96f578595cd71bf977a2f102d0b2d46e29c33fd8
Dev1Anim <89574788+Dev1Anim@users.noreply.github.com>
2024-06-25 17:39:12 +0800

    RAH

--- src/components/settingscomponent/SettingsPage.js
+++ src/components/settingscomponent/SettingsPage.js
@@ -180,9 +180,7 @@ function SettingsPage() {
                             </div>
                        </div>
                         <div className='flex flex-col w-[100%]'>
-                            <p className='text-[18px] md:text-[21px] font-medium mb-2'><svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
-</svg>
+                            <p className='text-[18px] md:text-[21px] font-medium mb-2'>
 Privacy & Account</p>
                             <div className='mx-3 bg-[#1a1a1f] text-xs font-bold px-5 py-3 rounded-lg flex items-center w-[100%] justify-between mb-3'>
                                 <div className='mr-4 w-[100%] ml-4 md:ml-6 mx-auto'>
