e6e1bee6eb801d1ac6dd40d754276971d2ee03c1
Dev1Anim <89574788+Dev1Anim@users.noreply.github.com>
2024-06-26 16:31:09 +0800

    fx

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,3 @@
-import { useEffect } from 'react';
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { NextUiProvider } from "./NextUiProvider";
@@ -13,6 +12,7 @@ import { Toaster } from 'sonner'
 import Changelogs from '../components/Changelogs';
 import FloatingButton from '@/components/FloatingButton';
 import { AuthProvider } from './SessionProvider';
+import ThemeManager from './ThemeManager';
 
 const inter = Inter({ subsets: ['latin'] })
 const myfont = localFont({ src: "../static-fonts/archivo.ttf" })
@@ -77,39 +77,9 @@ export const metadata = {
 export default async function RootLayout({ children }) {
   const session = await getAuthSession();
 
-
-  useEffect(() => {
-    // Check for the presence of a saved theme in the cookie when the component mounts
-    const savedTheme = getCookie("selectedTheme");
-    if (savedTheme) {
-      // Apply the saved theme to the site's CSS
-      if (savedTheme === 'purple') {
-        document.documentElement.style.setProperty('color', '#8e50cc'); // Update primary color CSS variable
-        document.documentElement.style.setProperty('background-color', '#2b183d');
-      } else if (savedTheme === 'blue') {
-        document.documentElement.style.setProperty('color', '#027AEF'); // Update primary color CSS variable
-        document.documentElement.style.setProperty('background-color', '#222F50');
-      } else if (savedTheme === 'green') {
-        document.documentElement.style.setProperty('color', '#02EF12'); // Update primary color CSS variable
-        document.documentElement.style.setProperty('background-color', '#305022');
-      } else if (savedTheme === 'pink') {
-        document.documentElement.style.setProperty('color', '#EF02ED'); // Update primary color CSS variable
-        document.documentElement.style.setProperty('background-color', '#4E2250');
-    } else if (savedTheme === 'yellow') {
-        document.documentElement.style.setProperty('color', '#F0C335'); // Update primary color CSS variable
-        document.documentElement.style.setProperty('background-color', '#504522');
-    } else if (savedTheme === 'red') {
-        document.documentElement.style.setProperty('color', '#C12727'); // Update primary color CSS variable
-        document.documentElement.style.setProperty('background-color', '#502222');
-    } else if (savedTheme === 'orange') {
-        document.documentElement.style.setProperty('color', '#D28525'); // Update primary color CSS variable
-        document.documentElement.style.setProperty('background-color', '#7B5B22');
-      }
-    }
-  }, []);
-
   return (
     <html lang="en" className='dark text-foreground bg-background' suppressHydrationWarning={true}>
+        <ThemeManager />
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=G-TYKL9CYSFS"
         strategy="afterInteractive"
@@ -163,8 +133,3 @@ export default async function RootLayout({ children }) {
     </html>
   )
 }
-
-const getCookie = (name) => {
-  const cookieValue = document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)');
-  return cookieValue ? cookieValue.pop() : '';
-};  
