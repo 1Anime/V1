90ed1184c4c4a23f477975dc109948ded1338185
Dev1Anim <89574788+Dev1Anim@users.noreply.github.com>
2024-06-28 19:31:24 +0800

    Replace

--- src/components/details/tabs/Overview.js
+++ src/components/details/tabs/Overview.js
@@ -138,7 +138,7 @@ function Overview({data}) {
             <span className="absolute pointer-events-none z-40 opacity-0 -translate-y-8 group-hover:-translate-y-10 group-hover:opacity-100 font-karla shadow-tersier shadow-md whitespace-nowrap bg-secondary px-2 py-1 rounded transition-all duration-200 ease-out">
               See on AniList
             </span>
-See on AL</a>
+            <MyAnimeListIcon className="w-7 h-7" /></a>
 <a
                             type="button"
                             target="_blank"
