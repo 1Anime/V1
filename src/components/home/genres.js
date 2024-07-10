9d0591ed556ce332a5da7a6bf6de59ba75c9baf2
Dev1Anim <89574788+Dev1Anim@users.noreply.github.com>
2024-07-10 18:13:22 +0800

    bryu

--- src/components/home/genres.js
+++ src/components/home/genres.js
@@ -42,10 +42,10 @@ function Genres ({}) {
       </div>
       <div className={styles.animeitems}>
         <span className={styles.leftarrow}>
-          <svg onClick={scrollLeft} xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.8" strokeLinecap="round" strokeLinejoin="round" className="mb-4"><path d="m15 18-6-6 6-6"></path></svg>
+          <svg  xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.8" strokeLinecap="round" strokeLinejoin="round" className="mb-4"><path d="m15 18-6-6 6-6"></path></svg>
         </span>
         <span className={styles.rightarrow}>
-          <svg onClick={scrollRight} xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.8" strokeLinecap="round" strokeLinejoin="round" className="mb-4"><path d="m9 18 6-6-6-6"></path></svg>
+          <svg  xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.8" strokeLinecap="round" strokeLinejoin="round" className="mb-4"><path d="m9 18 6-6-6-6"></path></svg>
         </span>
         <div className={styles.cardcontainer}>            {g.map((a, index) => (
               <Link
