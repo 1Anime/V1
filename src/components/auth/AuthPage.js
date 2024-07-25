2cc97c19959d00dd3811bc2b48772cb0a5184581
Dev1Anim <89574788+Dev1Anim@users.noreply.github.com>
2024-07-25 18:06:59 +0800

    try

--- src/components/auth/AuthPage.js
+++ src/components/auth/AuthPage.js
@@ -29,6 +29,22 @@ function AuthPage() {
                     </svg>
                     Sign up/Log In</div>
             </div>
+            <div className="bg-indigo-600">
+            <div className="max-w-screen-xl mx-auto px-4 py-3 items-center justify-between text-white sm:flex md:px-8">
+                <div className="flex gap-x-4">
+                    <div className="w-10 h-10 flex-none rounded-lg bg-indigo-800 flex items-center justify-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z" />
+                        </svg>
+                    </div>
+                    <p className="py-2 font-medium">
+                        Enjoy Anime, Novels & Manga for FREE without ads. Sync with your favorite account providers like AniList                    </p>
+                </div>
+                <a href="/" className="inline-block w-full mt-3 py-2 px-3 text-center text-indigo-600 font-medium bg-white duration-150 hover:bg-gray-100 active:bg-gray-200 rounded-lg sm:w-auto sm:mt-0 sm:text-sm">
+                    Use 1Anime now!
+                </a>
+            </div>
+        </div>
             {isLoggedIn ? (
             <div className='max-w-[94%] md:max-w-[95%] lg:max-w-[85%] xl:max-w-[80%] mx-auto min-h-[58vh] flex flex-col gap-6 mt-5'>
                                           <div className='mx-3 bg-[#1a1a1f] text-xs font-bold px-5 py-3 rounded-lg flex items-center w-[100%] justify-between'>
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -17,13 +17,7 @@ import { useRouter } from 'next-nprogress-bar';
 
 function Navbarcomponent({ home = false }) {
     const animetitle = useStore(useTitle, (state) => state.animetitle);
-    const Isopen = useStore(useSearchbar, (state) => state.Isopen);
-    const {
-        isOpen: isOpenModalOne,
-        onOpen: onOpenModalOne,
-        onClose: onCloseModalOne,
-      } = useDisclosure();
-      
+    const Isopen = useStore(useSearchbar, (state) => state.Isopen);  
       const {
         isOpen: isOpenModalTwo,
         onOpen: onOpenModalTwo,
@@ -354,7 +348,7 @@ function Navbarcomponent({ home = false }) {
                 <DropdownItem disabled key="manga" startContent={<BookOpenIcon className={iconClasses} />}>
                     <Link disabled href={`#`} className='w-full h-full block '>Manga (Unavailable)</Link>
                 </DropdownItem>
-                            <DropdownItem key="help_and_feedback" onPress={onOpenModalOne} startContent={<FeedbackIcon className={iconClasses} />}>Help & Feedback</DropdownItem>
+                            <DropdownItem key="help_and_feedback" onPress={onOpen} startContent={<FeedbackIcon className={iconClasses} />}>Help & Feedback</DropdownItem>
                             <DropdownItem key="settings" startContent={<SettingsIcon className={iconClasses} />}>
                                 <Link href={`/settings`} className='w-full h-full block '>Settings</Link>
                             </DropdownItem>
@@ -377,13 +371,13 @@ function Navbarcomponent({ home = false }) {
                                 <p className='text-[18px] md:text-[21px] font-medium'>Welcome to 1Anime!</p>
                                 <p className='text-[11px] md:text-[13px] text-[#bfc6d0]'> Please choose a way to login/signup</p>
                                 <button className="bg-white text-black font-medium py-2 px-3 rounded-lg"
-            onClick={onCloseModalTwo}>
-            Close
-          </button>           <button className="bg-white text-black font-medium py-2 px-3 rounded-lg"
                        onClick={() => signIn('AniListProvider')}
 >
 <svg fill="#000000" width="25px" height="25px" viewBox="0 0 24 24" role="img" xmlns="http://www.w3.org/2000/svg"><path d="M6.361 2.943 0 21.056h4.942l1.077-3.133H11.4l1.052 3.133H22.9c.71 0 1.1-.392 1.1-1.101V17.53c0-.71-.39-1.101-1.1-1.101h-6.483V4.045c0-.71-.392-1.102-1.101-1.102h-2.422c-.71 0-1.101.392-1.101 1.102v1.064l-.758-2.166zm2.324 5.948 1.688 5.018H7.144z"/></svg>  Login with AniList 
-          </button>  
+          </button>  <button className="bg-white text-black font-medium py-2 px-3 rounded-lg"
+            onClick={onCloseModalTwo}>
+            More Login providers coming soon.
+          </button>           
                             <div className='mr-4 w-full'>
                                 <p className='text-[18px] md:text-[21px] font-medium'>User Agreement</p>
                             <svg width="25px" height="25px" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
@@ -416,14 +410,14 @@ function Navbarcomponent({ home = false }) {
                     <Link disabled href={`#`} className='w-full h-full block '>Manga (Unavailable)</Link>
                 </DropdownItem>
                             </DropdownItem>
-                            <DropdownItem key="notloghelp_and_feedback" onPress={onOpenModalOne} startContent={<FeedbackIcon className={iconClasses} />}>Help & Feedback</DropdownItem>
+                            <DropdownItem key="notloghelp_and_feedback" onPress={onOpen} startContent={<FeedbackIcon className={iconClasses} />}>Help & Feedback</DropdownItem>
                             <DropdownItem key="settings" startContent={<SettingsIcon className={iconClasses} />}>
                                 <Link href={`/settings`} className='w-full h-full block '>Settings</Link>
                             </DropdownItem>
                         </DropdownMenu>
                     )}
                 </Dropdown>
-                <Feedbackform isOpen={isOpenModalOne} onOpen={onOpenModalOne} onOpenChange={onOpenChange} />
+                <Feedbackform isOpen={isOpen} onOpen={onOpen} onOpenChange={onOpenChange} />
             </div>
         </motion.nav>
     )
