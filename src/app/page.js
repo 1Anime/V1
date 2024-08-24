2156f5fa080ad1f58149f0fe5076b3266445f53f
Dev1Anim <89574788+Dev1Anim@users.noreply.github.com>
2024-08-24 16:38:48 +0800

    finalize(Remove V2 Ads): V1

--- src/app/page.js
+++ src/app/page.js
@@ -70,29 +70,6 @@ async function Home() {
         <RandomTextComponent />
           <ContinueWatching session={session} />
         </div>
-        <section className="relative overflow-hidden py-28 px-4 bg-gray-900 md:px-8">
-            <div className="w-full h-full rounded-full bg-gradient-to-r from-[#58AEF1] to-pink-500 absolute -top-12 -right-14 blur-2xl opacity-10"></div>
-            <div className="max-w-xl mx-auto text-center relative">
-                <div className="py-4">
-                    <h3 className="text-3xl text-gray-200 font-semibold md:text-4xl">
-                        1Anime 2.0 is almost here.. 
-                    </h3>
-                    <p className="text-gray-300 leading-relaxed mt-3">
-                      A big REVAMP is coming to 1Anime. Your feedbacks, Answered!~ Join the waitlist now!
-                    </p>
-                </div>
-                <div className="video-responsive">
-    <iframe
-      width="853"
-      height="480"
-      src={`https://www.youtube.com/embed/8X1mezu8RLc`}
-      frameBorder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowFullScreen
-      title="Trailer"
-    />
-  </div>            </div>
-        </section>
         <div
         >
           <RecentEpisodes cardid="Recent Episodes" />
